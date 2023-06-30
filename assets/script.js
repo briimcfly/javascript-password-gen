abf068271d870f44a2cef9f288260376c3053a11
Bryan Bickel <briimcfly@gmail.com>
2023-06-30 00:25:05 -0700

    added pseudocode

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Prompt user for password length 
+
+//Check to make sure the password length is between 8 and 128 Characters 
+
+//Prompt for lowercase characters
+
+//Prompt for uppercase characters
+
+//prompt for special characters 
+
+//prompt for numeric 
+
+//generate a password that matches the criteria 
+
+//write the password to the page 
