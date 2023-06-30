367e3360cc9e5dec2855e234fa7b38926aaea2e4
Bryan Bickel <briimcfly@gmail.com>
2023-06-30 00:47:48 -0700

    added arrays

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
 // Assignment code here
+alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+specialChar = ["!","@","#","$","%","^","&","*","(",")","-","+",".",",","?","_",";","'",'"',"[","]"];
+numbers = ["1","2","3","4","5","6","7","8","9","0"];
 
 
 // Get references to the #generate element
@@ -17,6 +20,9 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //Prompt user for password length 
+function generatePassword() {
+
+}
 
 //Check to make sure the password length is between 8 and 128 Characters 
 
